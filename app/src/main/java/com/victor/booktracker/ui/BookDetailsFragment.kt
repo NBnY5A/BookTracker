e8a9350258f5f192962987df1664fe865556aa4b
NBnY5A <victor.lito@a.ucb.br>
2026-05-02 12:38:41 -0300

    feat: Create BookDetailsFragment and book_detail_fragment layout

---
+++ app/src/main/java/com/victor/booktracker/ui/BookDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.victor.booktracker.ui
+
+import androidx.fragment.app.Fragment
+import com.victor.booktracker.R
+
+class BookDetailsFragment : Fragment(R.layout.book_detail_fragment) {
+
+}
