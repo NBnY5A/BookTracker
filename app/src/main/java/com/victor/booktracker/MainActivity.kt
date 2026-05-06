6d093b2edf95db2ec91a615e239b743e85aa8986
NBnY5A <victor.lito@a.ucb.br>
2026-05-06 20:48:38 -0300

    feat: Update MainActivity

--- app/src/main/java/com/victor/booktracker/MainActivity.kt
+++ app/src/main/java/com/victor/booktracker/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.lifecycle.ViewModelProvider
+import com.victor.booktracker.data.AppDatabase
+import com.victor.booktracker.data.repository.OfflineBookRepository
 import com.victor.booktracker.databinding.ActivityMainBinding
-import com.victor.booktracker.ui.fragments.book_list_fragment.BookListFragment
+import com.victor.booktracker.ui.fragments.book_list.BookListFragment
+import com.victor.booktracker.ui.view_model.BookViewModel
+import com.victor.booktracker.ui.view_model.BookViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,6 +29,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val db = AppDatabase.getInstance(this)
+        val repository = OfflineBookRepository(db!!.bookDao())
+
+        val viewModelFactory = BookViewModelFactory(repository)
+        val viewModel = ViewModelProvider(this, viewModelFactory)[BookViewModel::class.java]
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
