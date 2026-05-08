11904f55454631fd5959ab2eb7ddd25121f5283b
NBnY5A <victor.lito@a.ucb.br>
2026-05-08 12:26:22 -0300

    feat: Update AddBookFragment to include edit mode

--- app/src/main/java/com/victor/booktracker/ui/fragments/add_book/AddBookFragment.kt
+++ app/src/main/java/com/victor/booktracker/ui/fragments/add_book/AddBookFragment.kt
@@ -6,16 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.victor.booktracker.R
 import com.victor.booktracker.data.entity.BookEntity
 import com.victor.booktracker.databinding.AddBookFragmentBinding
 import com.victor.booktracker.ui.view_model.BookViewModel
+import kotlinx.coroutines.launch
+import kotlin.getValue
 
 class AddBookFragment : Fragment(R.layout.add_book_fragment) {
     private var _binding: AddBookFragmentBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: BookViewModel by activityViewModels()
+    private val args: AddBookFragmentArgs by navArgs()
+    private var isEditMode = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +38,32 @@ class AddBookFragment : Fragment(R.layout.add_book_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        if (args.bookId != -1) {
+            isEditMode = true
+            binding.btSaveBook.text = "Atualizar Livro"
+            viewModel.getBookById(args.bookId)
+            observeBookData()
+        }
+
         binding.btSaveBook.setOnClickListener {
             saveBook()
+        }
+    }
+
+    private fun observeBookData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.selectedBook.collect { book ->
+                    book?.let {
+                        binding.etBookName.setText(it.bookName)
+                        binding.etDescription.setText(it.description)
+                        binding.etAuthorName.setText(it.authorName)
+                        binding.etCategory.setText(it.category)
+                        binding.etTotalPages.setText(it.totalOfPages.toString())
+                        binding.etYearPublished.setText(it.yearOfPublished.toString())
+                    }
+                }
+            }
         }
     }
 
@@ -43,21 +76,33 @@ class AddBookFragment : Fragment(R.layout.add_book_fragment) {
         val year = binding.etYearPublished.text.toString().toIntOrNull() ?: 0
 
         if (name.isNotBlank() && pages > 0) {
-            val newBook = BookEntity(
-                bookId = 0,
-                bookName = name,
-                authorName = author,
-                description = description,
-                category = category,
-                totalOfPages = pages,
-                yearOfPublished = year,
-                lastPageRead = null,
-                isBookFinished = false
-            )
+            val book = if (isEditMode) {
+                viewModel.selectedBook.value?.copy(
+                    bookName = name,
+                    authorName = author,
+                    description = description,
+                    category = category,
+                    totalOfPages = pages,
+                    yearOfPublished = year
+                )
+            } else {
+                BookEntity(
+                    bookId = 0,
+                    bookName = name,
+                    authorName = author,
+                    description = description,
+                    category = category,
+                    totalOfPages = pages,
+                    yearOfPublished = year,
+                    lastPageRead = 0,
+                    isBookFinished = false
+                )
+            }
 
-            viewModel.insertBook(newBook)
-
-            requireActivity().supportFragmentManager.popBackStack()
+            book?.let {
+                if (isEditMode) viewModel.updateBook(it) else viewModel.insertBook(it)
+                findNavController().popBackStack()
+            }
         } else {
             binding.etBookName.error = "Informe o nome e as páginas"
         }
